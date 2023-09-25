d034a18ec55b0784134d78fc45bec84d45c72ca2
hahaharin <harin2950@naver.com>
2023-09-25 11:40:39 +0900

    feat: IoC 추가

---
+++ src/main/java/com/example/config/MyConfig.java
@@ -0,0 +1,10 @@
+package com.example.component;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyConfig {
+    @Bean
+    public void config(){}
+}
