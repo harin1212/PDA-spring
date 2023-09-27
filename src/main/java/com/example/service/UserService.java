4cc6da4a48e21a1c767a1d6b4cd3fe7f5855b43f
hahaharin <harin2950@naver.com>
2023-09-27 14:30:36 +0900

    feat: password 암호화

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -4,18 +4,19 @@ import com.example.domain.entity.User;
 import com.example.domain.request.UserRequest;
 import com.example.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void join(UserRequest userRequest) {
         User user = new User(
                 userRequest.getUsername(),
-                userRequest.getPassword(),
+                passwordEncoder.encode(userRequest.getPassword()),
                 userRequest.getRole()
         );
         userRepository.save(user);
